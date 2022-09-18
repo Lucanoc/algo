bfcbf8b6f3040cdfdfa19bdbc6f0ce7b3c3c1719
Lucanoc <321333424@qq.com>
2022-09-18 23:08:14 +0800

    添加项目文件。

---
+++ tmp.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <format>
+#include <vector>
+#include "sort/selectionSort.h"
+#include "sort/insertionSort.h"
+#include "sort/mergeSort.h"
+
+auto main() -> int {
+	std::vector<int> vec {1, 2, 3, 4, 8, 1, 2, 3, 1, 9};
+
+	dsa::mergeSort(vec.begin(), vec.end(), std::greater<int>());
+
+	for (int i : vec) {
+		std::cout << i << ' ';
+	}
+	std::cout << '\n';
+
+	return 0;
+}
