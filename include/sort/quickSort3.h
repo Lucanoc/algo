113d660217fc45b03c8bf0645baa33adbe4d68fc
Lucanoc <321333424@qq.com>
2022-09-20 14:14:40 +0800

    qSort update

--- include/sort/quickSort3.h
+++ include/sort/quickSort3.h
@@ -1,1 +1,31 @@
 #pragma once
+
+#include <utility>
+
+namespace dsa {
+	template <typename IteratorType, typename CompFuncType>
+	auto paritition3(IteratorType begin, IteratorType end, CompFuncType && cmp) -> std::pair<IteratorType> {
+		std::pair<IteratorType> bases(begin, end);
+		IteratorType moving{begin + 1};
+
+		while (moving != end) {
+
+		}
+
+		return bases;
+	}
+
+	template <typename IteratorType,
+		typename CompFuncType = std::less<
+		typename std::iterator_traits<IteratorType>::value_type>>
+		void quickSort3(IteratorType begin, IteratorType end, CompFuncType && cmp = CompFuncType{}) {
+		if (end - begin < 2) {
+			return;
+		}
+
+		std::pair<IteratorType> bases {dsa::partition3(begin, end, cmp)};
+
+		quickSort3(begin, bases.first, cmp);
+		quickSort3(bases.second, end, cmp);
+	}
+}
--- tmp.cpp
+++ tmp.cpp
@@ -4,11 +4,12 @@
 #include "sort/selectionSort.h"
 #include "sort/insertionSort.h"
 #include "sort/mergeSort.h"
+#include "sort/quickSort.h"
 
 auto main() -> int {
 	std::vector<int> vec {1, 2, 3, 4, 8, 1, 2, 3, 1, 9};
 
-	dsa::mergeSort(vec.begin(), vec.end(), std::greater<int>());
+	dsa::quickSort(vec.begin(), vec.end());
 
 	for (int i : vec) {
 		std::cout << i << ' ';
